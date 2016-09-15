ee7378c70ed8453aba8c4c77e46a188610116fbc
Dan F-M <danfm@nyu.edu>
2016-09-15 11:01:48 +0200

    adding gp worksheet

---
+++ gp/gp/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__all__ = ["line", "utils", "benchmark", "interactive"]
+
+from . import line
+from . import utils
+from .benchmarking import benchmark
