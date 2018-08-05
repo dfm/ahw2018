d909b463ea96759cc6951cc43c467b48d324cc2e
Dan F-M <danfm@nyu.edu>
2018-08-05 10:17:03 +0100

    edits to the paper

--- gp/gp/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__all__ = ["line", "utils", "benchmark", "interactive"]
-
-from . import line
-from . import utils
-from .benchmarking import benchmark
